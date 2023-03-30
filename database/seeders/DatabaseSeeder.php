e08daa70c9b54c67153f1ce6a0c9cf2fcfcfed5a
lucianoreguera <regueraluciano@gmail.com>
2023-03-30 12:45:36 -0300

    Módulo Productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategoriasSeeder::class
+            CategoriasSeeder::class,
+            ProductosSeeder::class
         ]);
     }
 }
